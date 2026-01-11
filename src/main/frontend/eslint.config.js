b69fb8a3f87e79c1d25774838cdf585b0463b798
Grzegorz Puścizna <grzesiekpuscizna@gmail.com>
2026-01-11 20:28:58 +0100

    Add vite and eslint config

--- src/main/frontend/eslint.config.js
+++ src/main/frontend/eslint.config.js
@@ -3,21 +3,42 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import {defineConfig, globalIgnores} from 'eslint/config'
+import reactX from 'eslint-plugin-react-x'
+import reactDom from 'eslint-plugin-react-dom'
 
 export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    globalIgnores(['dist']),
+    {
+        files: ['**/*.{ts,tsx}'],
+        extends: [
+            js.configs.recommended,
+            tseslint.configs.recommendedTypeChecked,
+            // tseslint.configs.strictTypeChecked, // TODO: try it some day, seems pretty strict
+            tseslint.configs.stylisticTypeChecked,
+
+            reactX.configs['recommended-typescript'],
+            reactDom.configs.recommended,
+        ],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+
+            parserOptions: {
+                project: ['./tsconfig.node.json', './tsconfig.app.json'],
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+        plugins: {
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+        },
+        rules: {
+            ...reactHooks.configs.recommended.rules,
+            'react-refresh/only-export-components': [
+                'warn',
+                {allowConstantExport: true},
+            ],
+        },
     },
-  },
 ])
--- src/main/frontend/vite.config.ts
+++ src/main/frontend/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Build files will go to static resources of Spring app
+    outDir: '../resources/static',
+    // Clean directory before every build
+    emptyOutDir: true,
+  },
+  server: {
+    proxy: {
+      // Proxy requests to Spring during development
+      '/api': 'http://localhost:8080'
+    }
+  }
 })
