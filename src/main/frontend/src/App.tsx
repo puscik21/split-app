b014cf27b682453be3b9efe76348b0f75ddf8ce6
Grzegorz Puścizna <grzesiekpuscizna@gmail.com>
2026-01-17 12:37:23 +0100

    Small cleanup

--- src/main/frontend/src/App.tsx
+++ src/main/frontend/src/App.tsx
@@ -1,41 +1,16 @@
-import {useState} from "react"
-import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
-import "./App.css"
-import {fetchUsersRequest} from "./api/userApi.ts";
+import {CssBaseline, ThemeProvider} from '@mui/material';
+import {darkTheme} from "./theme/theme.ts";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  const handleClick = () => {
-    setCount((count: number) => count + 1)
-    console.log(fetchUsersRequest())
-  }
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo"/>
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo"/>
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={handleClick}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline/>
+      {/*<GameContextProvider>*/}
+      {/*  <GameContent/>*/}
+      {/*</GameContextProvider>*/}
+      <h1>Hello</h1>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
